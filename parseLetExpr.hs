01ae3062f042e589a2757f5b47557977ea8b7df7
yiyu zhong <zhong_yi_yu@outlook.com>
2023-07-19 13:09:50 +0100

    Add "Let .. In" expressions

--- parseLetExpr.hs
+++ parseLetExpr.hs
@@ -2,10 +2,7 @@ import Text.Parsec
 import Text.Parsec.String (Parser)
 import Language.Haskell.TH.Syntax (NameSpace(VarName))
 
--- data LetStmt = Let String Expr  Type
-    -- deriving (Show)
-
-data Expr = Var String | Int Int | String String | Let String Expr Type
+data Expr = Var String | Int Int | String String | Let String Expr Type| LetIn String Expr Type Expr
     deriving (Show)
 
 data Type = IntType | StringType | UnknownType
@@ -17,28 +14,42 @@ letExpression = do
   spaces
   varName <- many1 letter
   spaces
---   If there's no ":" symbol, we assume the type is UnknownType
   varType <- typeParser
---   varType = option UnknownType (try (do 
---     char ':'
---     spaces
---     typeParser
---     ))
   spaces
   char '='
   spaces
   expr <- expression
-  return (Let varName expr varType)
+  inexpr <- inExprParser
+  return (Let varName expr varType )
+
+letInExpression :: Parser Expr
+letInExpression = do
+  string "let"
+  spaces
+  varName <- many1 letter
+  spaces
+  varType <- typeParser
+  spaces
+  char '='
+  spaces
+  expr <- expression
+  LetIn varName expr varType <$> inExprParser
 
 -- 通过解析是否有":"符号判断是否有类型，如果没有符号，就返回UnknownType
 typeParser :: Parser Type
 typeParser = choice [string ":Int" >> return IntType, string ":String" >> return StringType, return UnknownType]
 
+inExprParser :: Parser Expr
+inExprParser = do
+    spaces
+    string "in"
+    spaces
+    expression
 
 expression :: Parser Expr
 expression = do
-  varName <- many1 letter <|> many1 digit
-  return (Var varName)
+    varName <- many1 letter <|> many1 digit
+    return (Var varName)
 
 parseLetStatement :: String -> Either ParseError Expr
 parseLetStatement = parse letExpression ""
