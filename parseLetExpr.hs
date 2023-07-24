34bc6721609d9042777cac0219c78f9a0a757dcb
zhong-yiyu <53254770+zhong-yiyu@users.noreply.github.com>
2023-07-24 13:14:24 +0100

    tmp fix

--- parseLetExpr.hs
+++ parseLetExpr.hs
@@ -1,21 +1,24 @@
-import Language.Haskell.TH.Syntax (NameSpace (VarName))
+import Language.Haskell.TH.Syntax (NameSpace (VarName), Con)
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
 data Expr
   = Var String
   | Int Int
-  | String String
   | Let String Expr Type
   | LetIn String Expr Expr
   | VarType String Type
   | ExprAdd Expr Expr
   deriving (Show)
 
-
-
-data Type = IntType | StringType | UnknownType
+newtype TypeEnv = TypeEnv [(String, Type)]
   deriving (Show)
+
+combineTypeEnv :: TypeEnv -> TypeEnv -> TypeEnv
+combineTypeEnv (TypeEnv env1) (TypeEnv env2) = TypeEnv (env1 ++ env2)
+
+data Type = IntType | UnknownType | UnitType| LetType
+  deriving (Show, Eq)
 
 letExpression :: Parser Expr
 letExpression = do
@@ -46,7 +49,7 @@ letInExpression = do
 
 -- By detecting the ":" character, we can determine if the type has an annotation
 typeParser :: Parser Type
-typeParser = choice [string ":Int" >> return IntType, string ":String" >> return StringType, return UnknownType]
+typeParser = choice [string ":Int" >> return IntType, return UnknownType]
 
 inExprParser :: Parser Expr
 inExprParser = do
@@ -85,3 +88,28 @@ checkExpr (VarType _ _) _ = True
 
 -- 在ghci中测试一下内容，应该为True
 -- checkExpr (Int 1) IntType
+
+synthExpr :: Expr -> (Type, TypeEnv)
+synthExpr (Let {}) = (LetType, TypeEnv [])
+synthExpr (ExprAdd e1 e2) = 
+  let env1 = checkExprWithEnv e1 IntType in
+    let env2 = checkExprWithEnv e2 IntType in
+      (IntType, combineTypeEnv env1 env2)
+synthExpr (VarType varName varType) = (varType, TypeEnv [(varName, varType)])
+synthExpr (Int _) = (IntType, TypeEnv [])
+synthExpr _ = error "Not implemented"
+
+checkExprWithEnv :: Expr -> Type -> TypeEnv
+-- checkExprWithEnv = undefined
+checkExprWithEnv (Var varName) varType = TypeEnv [(varName, varType)]
+-- checkExprWithEnv (ExprAdd e1 e2) varType = 
+-- checkExprWithEnv (Let varname expr1 expr2) varType = 
+  -- let env1 = checkExprWithEnv expr1 varType in
+    -- let checkTy = find varname env1 in
+      -- let env2 = checkExprWithEnv expr2 checkTy in
+        -- env1 ++ env2
+-- This function is the "default" or "catch-all" case, normally used as last resort
+checkExprWithEnv e ty = 
+  let (synthTy, env) = synthExpr e in
+    if synthTy == ty then env else error "Type mismatch"
+-- let exampleAddExpr = ExprAdd (Int 1) (Int 2)
